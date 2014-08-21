626486800f024765169be80b5c08dbebaf34cabd
Tudor Laptes <tudorlaptes@gmail.com>
2014-08-21 01:09:40 -0400

    updated comment section

--- src/stringtransposition/StringTransposition.java
+++ src/stringtransposition/StringTransposition.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 /**
  *
  * @author todyerutz @plainintricacy.wordpress.com
+ * A simple program that inputs multiple strings and outputs them vertically.
+ * Based on this reddit daily challenge: http://tinyurl.com/lfdum9s
+ * 
  */
+ 
+ 
 public class StringTransposition {
 
    
